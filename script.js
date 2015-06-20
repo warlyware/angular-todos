e79e7a94fedf39ea975b64dba73dc202d6c67d22
warlyware <danward@gmail.com>
2015-06-20 15:37:01 -0700

    Add inline editing of tasks

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-var TODOS = [
-  { action: "Do this first", done: false },
-  { action: "Learn HB", done: true, funky: true },
-  { action: "Learn Angular", done: false, important: true },
-  { action: "Learn HB", done: true },
-  { action: "Learn Angular", done: false },
-  { action: "Learn HB", done: true, funky: true },
-  { action: "Learn Angular", done: false, important: true },
-  { action: "Learn HB", done: true },
-  { action: "Learn Angular", done: false }
-];
-
 angular.module("todosApp", [])
 .controller("todoController", function($scope) {
-  $scope.todos = TODOS;
+  $scope.todos = [
+    { action: "Do this first", done: false, editing: false },
+    { action: "Learn HB", done: true, funky: true },
+    { action: "Learn Angular", done: false, important: true, editing: true },
+    { action: "Learn HB", done: true, editing: false },
+    { action: "Learn Angular", done: false, editing: false },
+    { action: "Learn HB", done: true, funky: true, editing: false },
+    { action: "Learn Angular", done: false, important: true, editing: false },
+    { action: "Learn HB", done: true, editing: false },
+    { action: "Learn Angular", done: false, editing: false }
+    ];
+  
+  $scope.clearCompleted = function() {
+    $scope.todos = $scope.todos.filter(function(e) {
+      return !e.done;
+    });  
+  };
+  
+  $scope.markAll = function() {
+    $scope.todos = $scope.todos.map(function(e) {
+      e.done = $scope.allDone;
+      return e;
+    });
+  };
+
+  $scope.editTodo = function(todoEvt) {
+    console.log(todoEvt.target);
+    todoEvt.target.hide();
+    // var prompt = window.prompt('Change: ' + todo.action, todo.action);
+    // todo.action = prompt;   
+  }
+  
 });
