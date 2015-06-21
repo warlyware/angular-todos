6d304291c85d7635ded330e2c102f0cb331f2c25
warlyware <danward@gmail.com>
2015-06-20 17:07:33 -0700

    Add basic styles, search functionality

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ angular.module("todosApp", [])
 .controller("todoController", function($scope) {
   $scope.todos = [
     { action: "Do this first", done: false, editing: false },
-    { action: "Learn HB", done: true, funky: true },
-    { action: "Learn Angular", done: false, important: true, editing: true },
+    { action: "Learn HB", done: true, funky: true, editing: false },
+    { action: "Learn Angular", done: false, important: true, editing: false },
     { action: "Learn HB", done: true, editing: false },
     { action: "Learn Angular", done: false, editing: false },
     { action: "Learn HB", done: true, funky: true, editing: false },
@@ -18,18 +18,28 @@ angular.module("todosApp", [])
     });  
   };
   
-  $scope.markAll = function() {
-    $scope.todos = $scope.todos.map(function(e) {
-      e.done = $scope.allDone;
-      return e;
-    });
-  };
-
   $scope.editTodo = function(todoEvt) {
     console.log(todoEvt.target);
     todoEvt.target.hide();
     // var prompt = window.prompt('Change: ' + todo.action, todo.action);
     // todo.action = prompt;   
   }
+
+  $scope.$watch('allDone', function(newValue) {
+    if (angular.isUndefined(newValue)) {
+      return;
+    }
+    $scope.todos = $scope.todos.map(function(e) {
+      e.done = $scope.allDone;
+      return e;
+    });
+  });
+
+  $scope.$watch('secret', function(newValue) {
+    if (newValue == 'angular') {
+          document.body.style.backgroundColor = newValue === 'angular' ? 'green' : 'white';
+    }
+  });
   
 });
+
